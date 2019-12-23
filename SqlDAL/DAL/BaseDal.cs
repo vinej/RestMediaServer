63a5a060b4c75ba1007d67dfd9b7c77c53dc0332
jyv <jyvinet@hotmail.ca>
2019-12-23 11:16:57 -0500

    SqlData

---
+++ SqlDAL/DAL/BaseDal.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using SqlDAL.Core;
+
+namespace SqlDAL.DAL
+{
+    public class BaseDal
+    {
+        public SqlHelper sqlHelper = null;
+        public SqlConnection connection = null;
+
+        public BaseDal()
+        {
+            sqlHelper = new SqlHelper(ConfigurationManager.ConnectionStrings["DBConnection"].ToString());
+        }
+
+        public void CloseConnection()
+        {
+            sqlHelper.CloseConnection(connection);
+        }
+    }
+}
